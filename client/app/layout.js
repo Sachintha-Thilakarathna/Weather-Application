310dd3ace391b7738df9f5a5dba6c7126aa12e84
sachintha <sachintha@sachintha.sachintha>
2026-08-28 15:05:21 +0530

    Initial commit

---
+++ client/app/layout.js
@@ -0,0 +1,31 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Weather Application",
+  description: "A simple weather application built with Next.js,Nest.js and Tailwind CSS.",
+  icons:{
+  icon: '/Logo.png'
+  }
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
